2264d502bce6d519967ce832d6f962ddb9b84d64
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2026-05-20 16:16:12 +0200

    migrazione del database

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,5 +4,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+// Configuriamo le opzioni per includere il tuo schema personalizzato
+const options = {
+  db: {
+    schema: 'terzo-tempo-app',
+  },
+};
+
 // Creiamo l'istanza di connessione
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, options);
